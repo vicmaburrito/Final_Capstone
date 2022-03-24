f6766027652cf97308a589c11091ec06eef1f117
angelacuahutle <angela.cuahutle@gmail.com>
2022-03-24 14:08:44 -0600

    Configure Rails app for cookies, the user model and sessions controllerand react components

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Specify a serializer for the signed and encrypted cookie jars.
-# Valid options are :json, :marshal, and :hybrid.
-Rails.application.config.action_dispatch.cookies_serializer = :json
-
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: '_name-of-your-app', domain: 'name-of-you-app-json-api'
+else
+  Rails.application.config.session_store :cookie_store, key: '_name-of-your-app'
+end
